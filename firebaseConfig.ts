86df4d28c5f1072cddb96e190ec702319e85fd5b
DastanSmagulov <smagulovdastan07@gmail.com>
2024-08-21 09:44:39 +0500

    feat: add dotenv variables

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyARhE9lrJ2kEw8uxeQugq_j2MDr4dbaRi8",
-  authDomain: "test-expo-ab290.firebaseapp.com",
-  projectId: "test-expo-ab290",
-  storageBucket: "test-expo-ab290.appspot.com",
-  messagingSenderId: "487084301916",
-  appId: "1:487084301916:web:3efbd8ac3dd938ab261099",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
